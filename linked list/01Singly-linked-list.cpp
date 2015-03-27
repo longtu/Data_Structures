feb16c46ce4e69e89799198b852d08fe161d5b42
vo01github <hxt2014bbg@icloud.com>
2015-03-27 18:32:42 +0800

    1

--- linked list/01Singly-linked-list.cpp
+++ linked list/01Singly-linked-list.cpp
@@ -10,24 +10,154 @@
     那么在实际应用中，链表是怎么设计的呢？我们可以以int数据类型作为基础，设计一个简单的int链表：
 */
 
+#define FALSE 	0
+#define TRUE    1
+
 // __________________________1 设计链表的数据结构 __________________________
 typedef struct NODE
 {
-	struct NODE *link;
-	int			value;
+	int				value;
+	struct NODE *	next;
 }Node;
 
 
-// __________________________2  创建链表__________________________
+
+// __________________________2  创建节点__________________________
 
 Node * create()
 {
-	Node * newNode;
+	Node * newNode = NULL;
 	newNode = (Node *)malloc(sizeof(Node));
-	if ( newNode != NULL )
-	{
-		return newNode;
-	}
+	assert(newNode != NULL);
+
+	newNode->next = NULL;
+	return newNode;
 }
 
-// __________________________3  创建链表__________________________
+
+// __________________________3  链表里面插入值 __________________________
+// 程序12.3 插入到一个有序的单链表：最终版本【来源于《C和指针》的习题】
+int thr_insert(Node  **pRoot, int new_value)
+{
+	Node *current = *pRoot;			// 这条语句对根指针参数执行间接访问操作，得到的结果是root的值
+	Node *newNode;
+
+
+	// 寻找正确的插入位置，方法是按顺序访问链表，直到到达其值大于或等于新插入值的节点
+	while( ( NULL != current )&&	// 如果到达 尾节点了。
+		( current->value < new_value ) )
+	{
+		pRoot = &current->link;
+		current = *pRoot;
+	}
+
+	// 为新节点分配内存，并把新值存储到新节点中，如果内存分配失败，函数返回 false.
+	newNode = (Node *)malloc(sizeof(Node));
+	if ( newNode == NULL)return FALSE;
+	newNode->value = new_value;
+
+	// 把新节点插入到链表中，并返回TRUE
+	newNode->link = current;		// 即使 current 是NULL也没有关系了
+	*pRoot = newNode;
+	return TRUE;
+} 
+
+result = thr_insert(&root,12);
+
+
+
+// __________________________4  删除链表 __________________________
+
+
+
+
+
+
+// __________________________5  查找数据 __________________________
+Node* findDate(const Node* pRoot,int value)
+{
+	while(pRoot != NULL)
+	{
+		if( pRoot->value == value )break;
+		pRoot = pRoot->next;
+	}
+
+	return pRoot;		// 返回NULL 就是没有找到 
+}
+
+
+// __________________________6 删除数据所在节点 __________________________
+enum{
+	NO_DELATE_OPERATION;
+	DELATE_HAPPEN;
+}
+int delateDate(const Node* pRoot,int value)		// 这个函数 无法解决 要删除 链表头节点的情况
+{
+	Node* father 		= pRoot;
+	int delate_result 	= NO_DELATE_OPERATION;
+	while(pRoot != NULL)
+	{
+		if( pRoot->value == value )
+		{
+			father->next = pRoot->next;
+			free(pRoot);
+			pRoot 	= father->next;
+			delate_result 	= DELATE_HAPPEN;
+		}
+		father = pRoot;
+		pRoot = pRoot->next;
+	}
+
+	return delate_result;						// 0说明没有进行删除数据的操作。1说明进行了删除数据的操作。
+}
+
+
+int fir_delete(Node  **pRoot, int delete_value)		// 可以删除头节点的版本 【来源于《C和指针》的习题】
+{
+	Node *current = *pRoot;			// 这条语句对根指针参数执行间接访问操作，得到的结果是root的值
+	Node *delete;
+
+	// 寻找 值相同的 节点____________________________________________________
+	while( ( NULL != current )&&	// 如果到达 尾节点了。
+		( current->value != delete_value) )
+	{
+		pRoot = &current->link;
+		current = *pRoot;
+	}
+
+	// 没有找到要删除的值
+	if( NULL == current ) return FALSE;
+
+	//
+	delete = *pRoot;
+	*pRoot = current->link;
+
+	// 把删除节点释放，并返回TRUE,这样free到底可不可以啊
+	free(delete);
+	return TRUE;
+} 
+
+
+// __________________________8  计数一个单向链表的节点数 __________________________
+// 递归版本
+int sum(const Node* pRoot)
+{
+	if(pRoot == NULL)return 0;
+
+	return 1 + sum(pRoot->next);
+}
+
+// for循环版本
+int sum_node(const Node* pRoot)
+{
+	int sum; 
+	for(sum = 0; pRoot != NULL; sum++ )
+	{
+		pRoot = pRoot->next;
+	}
+	return sum;
+}
+
+
+
+
